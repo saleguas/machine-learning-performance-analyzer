bd75552a7ef9e2b843ae88c1af62e774caf63c22
salvador aleguas <salvadoraleguas@gmail.com>
2019-12-18 02:59:06 -0800

    Changed entire code

--- src/scripts/downloadData.py
+++
@@ -1,9 +0,0 @@
-import quandl
-import os
-
-
-def downloadData(ticker):
-    print("Downloading {}...".format(ticker))
-    df = quandl.get(ticker)
-    filename = ''.join([ticker, '.csv']).replace('/', '_')
-    df.to_csv(os.path.join('.', 'data', filename))
